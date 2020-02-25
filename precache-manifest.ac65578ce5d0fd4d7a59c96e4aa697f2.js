b453adfed107935e95117d4b91073562d4757732
Wes Moses <wesmoses@gmail.com>
2020-02-26 01:05:49 +0200

    Updates

---
+++ precache-manifest.ac65578ce5d0fd4d7a59c96e4aa697f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0e466847abcdf1f825a3888b684743",
+    "url": "/cat-fact-fetcher/index.html"
+  },
+  {
+    "revision": "4d49d3ca8bc7de17c1fb",
+    "url": "/cat-fact-fetcher/static/css/main.e21d3fdb.chunk.css"
+  },
+  {
+    "revision": "b4051d03e074a88dba78",
+    "url": "/cat-fact-fetcher/static/js/2.06ac58ee.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cat-fact-fetcher/static/js/2.06ac58ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d49d3ca8bc7de17c1fb",
+    "url": "/cat-fact-fetcher/static/js/main.1d8be7e1.chunk.js"
+  },
+  {
+    "revision": "56fea29f1bff57dd1f37",
+    "url": "/cat-fact-fetcher/static/js/runtime-main.ad88322f.js"
+  }
+]);
